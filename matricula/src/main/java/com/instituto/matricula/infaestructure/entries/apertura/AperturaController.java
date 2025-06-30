558dc231491f74906af2bbfd2d43725ce9e4281f
Jorge Pantigoso <jnio@intercorp.com.pe>
2025-06-30 15:41:03 -0500

    Feature:Api Gateway

--- matricula/src/main/java/com/instituto/matricula/infaestructure/entries/apertura/AperturaController.java
+++ matricula/src/main/java/com/instituto/matricula/infaestructure/entries/apertura/AperturaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/instituto/apertura/")
+@RequestMapping("/api/v1/instituto/matricula/apertura/")
 public class AperturaController {
 
     private final AperturaService aperturaService;
--- matricula/src/main/java/com/instituto/matricula/infaestructure/entries/curso/CursoController.java
+++ matricula/src/main/java/com/instituto/matricula/infaestructure/entries/curso/CursoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/instituto/curso/")
+@RequestMapping("/api/v1/instituto/matricula/curso/")
 public class CursoController {
 
     private final CursoService cursoService;
